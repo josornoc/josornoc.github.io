58ee5569a2bc85f049a0c630d4e12bb4f145acec
josornoc <jose_osorno@hotmail.com>
2014-12-25 22:04:45 -0500

    word organizer exercise added

---
+++ ruby/wordSorting/wordSorting.rb
@@ -0,0 +1,17 @@
+def sortWords(str)
+	str = str.gsub(/[^a-z0-9\s]/i, '')
+	words = str.split(" ")
+	words = compareWords(words)
+end
+
+def compareWords(strArr)
+	strArr = strArr.sort_by{|word| word.downcase}
+end
+
+def wordOrganizer()
+	puts "Enter the phrase to organize the words ignoring case...."
+	puts sortWords(gets.chomp.to_s)
+	wordOrganizer()
+end
+
+wordOrganizer()
