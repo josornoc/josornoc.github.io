4b44b06ebeeff23db302a86dc58d0c96d30265f3
josornoc <jose_osorno@hotmail.com>
2015-01-17 21:15:04 +0100

    route class added

--- course/ChessValidator/chessValidator.rb
+++ course/ChessValidator/chessValidator.rb
@@ -30,6 +30,9 @@ class Piece
 	def initialize(side)
 		@side = side
 	end
+	def method_missing(m, *args, &block)
+		puts "Method called '" + m.to_s + "' doesn't exists in this Piece yet..."
+	end
 end
 
 class King < Piece
@@ -60,19 +63,18 @@ class Peon < Piece
 	def initialize(side)
 		super(side)
 	end
-
 end
 
 
 class ChessMovesValidator
 
 	def initialize( boardFileName, movesFileName )
+		@chessBoard = ChessBoard.new
 		@movesCollection = []
-		parse_chess_file(boardFileName,movesFileName)		
+		parse_chess_files(boardFileName,movesFileName)		
 	end
 
-	def parse_chess_file( boardFileName, movesFileName )
-		@chessBoard = ChessBoard.new
+	def parse_chess_files( boardFileName, movesFileName )
 		process_board(boardFileName)
 		process_moves(movesFileName)
 	end
@@ -119,7 +121,6 @@ class ChessBoard
 	def initialize
 		@boardSideSize = 0;
 		@boardLineCollection = []
-		@boardSpacesCollection = []
 	end
 
 	def create_board_line(boardLine)
@@ -137,10 +138,6 @@ class ChessBoard
 		piecesClassArray
 	end
 
-	def check_board_lines_collection
-		print @boardLineCollection
-	end
-
 	def validate_movement(mvm)
 		#RETURN TRUE IS VALID AND FALSE IF INVALID
 		pieceOnFirstPosition = get_piece_on_position(mvm.initPos)
@@ -156,54 +153,76 @@ class ChessBoard
 		#if a friend is mvm is invalid
 		#if Knight this is overriden, no matter what....
 		#if a foe is on route, not in final position the mvm is also invalidated
-		piece.get_route_positions_array(firstPos, finalPos)
+		puts "Get_piece_movement_route "
+		gRouteArray = piece.get_route_positions_array(firstPos, finalPos)
+		if(gRouteArray != nil)
+			gRouteArray.each do |position|
+				p position
+			end
+		else
+			pust "gRouteArray is nil"
+		end
 	end
 
 	def eval_if_different_sides(piece1, piece2)
-		piece2 == nil || piece1.side != piece2.side
+		piece1.side != piece2.side
 	end
 
-	def get_piece_on_position(iPosition)
-		firstChar = iPosition.split("")[0]
+	def get_piece_on_position(pos)
+		firstChar = pos.split("")[0]
 		pieceCoords = []
-		pieceCoords[0] = CoordConverter.get_number_of_string_char(firstChar)
-		lastChar = iPosition.split("")[1].to_i
+		pieceCoords[0] = CoordConverter.get_number_of_stringchar(firstChar)
+		lastChar = pos.split("")[1].to_i
 		pieceCoords[1] = lastChar - 1
 		@boardLineCollection[pieceCoords[1]][pieceCoords[0]]
 	end
-
-	def check_piece_movement_rules(piece, p1, p2)
-		piece.evaluate_mvm_rules(p1, p2)
-	end	
 end
 
 class Rook < Piece
 	def initialize(side)
 		super(side)
-	end
-	def evaluate_mvm_rules(p1, p2)
-		booleanReturn = false
-		if(p1[0] == p2[0])
-			booleanReturn = true
-		elsif (p1[1] == p2[1])
-			booleanReturn = true
-		else
-			booleanReturn = false
-		end
-		booleanReturn
-	end
+	end	
 	def get_route_positions_array(p1, p2)
-		puts "get_route_positions_array"	
-		puts "column: " + CoordConverter.get_number_of_string_char(p1[0]).to_s
-		puts "row: " + p1[1].to_s
+		fPosCoords = [CoordConverter.get_number_of_stringchar(p1[0]), p1[1].to_i - 1]
+		fPos2Coords = [CoordConverter.get_number_of_stringchar(p2[0]), p2[1].to_i - 1]
+		gRouteArray = Routes.get_route_from_coords(fPosCoords, fPos2Coords, ["horizontal", "vertical"])
+		gRouteArray
+	end
+end
+
+class Routes
+	def self.get_route_from_coords(coord1, coord2, rTypes)
+		if coord1[0] == coord2[0] && rTypes.index("vertical") > -1
+			get_vertical_route(coord1, coord2)
+		else
+			nil
+		end
+	end
+	def self.get_vertical_route(coord1, coord2)
+		if(coord1[1] > coord2[1])
+			tArray = (coord2[1]..coord1[1]).to_a
+			tArray = tArray.reverse
+		else
+			tArray = (coord1[1]..coord2[1]).to_a
+		end
+		tmpArray = []
+		tArray.each do |i|
+			tmpArray << CoordConverter.get_stringchar_of_number(coord1[0])+(i+1).to_s
+		end
+		tmpArray
 	end
 end
 
 class CoordConverter
-	def self.get_number_of_string_char(stringChar)
+	def self.get_number_of_stringchar(stringChar)
 		charArray = ("a".."z").to_a
 		rCoordNum = charArray.index(stringChar)
 		rCoordNum
+	end
+	def self.get_stringchar_of_number(number)
+		charArray = ("a".."z").to_a
+		rCoordStr = charArray[number]
+		rCoordStr
 	end
 end
 
