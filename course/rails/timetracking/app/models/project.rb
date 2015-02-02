2ef57051cc32cbdb9d6fc148dd9f997b8031dc2d
josornoc <jose_osorno@hotmail.com>
2015-02-02 15:25:24 +0100

    Destroy all old projects using date / created_at

--- course/rails/timetracking/app/models/project.rb
+++ course/rails/timetracking/app/models/project.rb
@@ -5,4 +5,9 @@ class Project < ActiveRecord::Base
 	def self.get_by_id(n)
 		where("id = ?", n)
 	end
+
+	def self.clean_old()
+		from = Date.today - 1.week
+		where("created_at < ?", from).destroy_all
+	end
 end
