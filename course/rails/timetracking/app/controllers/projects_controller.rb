bf3571afb7f0ffacfba3960d86953c8c3a50e991
josornoc <jose_osorno@hotmail.com>
2015-02-02 17:43:12 +0100

    params to URL added, exceptions controller to not founded projects

--- course/rails/timetracking/app/controllers/projects_controller.rb
+++ course/rails/timetracking/app/controllers/projects_controller.rb
@@ -3,5 +3,12 @@ class ProjectsController < ApplicationController
 		@yMama = "your mama"
 		@projects = Project.last_created_projects(10)
 	end	
+	def show
+		begin
+			@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			render 'no_projects_found'
+		end
+	end
 end
 
