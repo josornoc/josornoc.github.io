3c7cc3b0d5c57e288c79b23cff2ab907cf24ca43
josornoc <jose_osorno@hotmail.com>
2015-02-04 17:24:46 +0100

    finish testing confirming when deleting an entry

--- course/rails/timetracking/app/controllers/entries_controller.rb
+++ course/rails/timetracking/app/controllers/entries_controller.rb
@@ -3,14 +3,14 @@ class EntriesController < ApplicationController
 		@project = Project.find params[:project_id]
 		@entries = @project.entries
 		#@entriesThisMonth = @project.entries_last_month
-		@entriesThisMonth = @project.entries_by_year_and_month(2015, 2)
+		@entriesThisMonth = @project.entries_by_year_and_month(Date.today.year, Date.today.month)
 		@hoursWorked = 0
 		@minutesWorked = 0
 		@entriesThisMonth.each do |entry|
 			@hoursWorked += entry.hours
 			@minutesWorked += entry.minutes
 		end
-		@hoursWorked += @minutesWorked / 60
+		@hoursWorked += (@minutesWorked.to_f / 60).round(2)
 	end
 
 	def new
@@ -32,10 +32,30 @@ class EntriesController < ApplicationController
 	def edit
 		@project = Project.find params[:project_id]
 		@entry = @project.entries.find params[:id]
-		#@entry = @project.entries.new entry_params
+		render 'new'
+	end
+
+	def update
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+		if @entry.update_attributes entry_params
+			redirect_to "/projects/" + @project.id.to_s + "/entries"
+		else
+			@errors	=	obj.errors.full_messages
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.find params[:id]
+		@entry.destroy
+		@entry.save
+		redirect_to "/projects/" + @project.id.to_s + "/entries"
 	end
 
 	private
+
 	def entry_params
 		params.require(:entry).permit(:hours, :minutes, :date, :comments)
 	end
