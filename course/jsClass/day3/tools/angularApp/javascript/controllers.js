4655aeacebf079749ab728583e201731eaa8a180
josornoc <jose_osorno@hotmail.com>
2015-02-11 16:57:09 +0100

    Using external API to get object for Angular APP

---
+++ course/jsClass/day3/tools/angularApp/javascript/controllers.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* Controllers */
+var peopleApp = angular.module('angularApp', []);
+
+peopleApp.controller('PeopleListCtrl', function ($scope, $http) {
+
+	$http.get('http://tinder-api.herokuapp.com/people').success(function(data) {
+    $scope.people = data;
+  });
+
+  // $scope.people = [
+  //   {'name': 'Adrià',
+  //    'avatar': 'http://xoart.link/200/200/man/12',
+  //    'description': 'On the weekends I play cricket.'},
+  //   {'name': 'Moritz',
+  //    'avatar': 'http://xoart.link/200/200/woman/22',
+  //    'description': 'I like pizza and icecream.'},
+  //   {'name': 'Hector',
+  //    'avatar': 'http://xoart.link/200/200/man/26',
+  //    'description': 'Amazing double rainbow.'}
+  // ]
+
+});
