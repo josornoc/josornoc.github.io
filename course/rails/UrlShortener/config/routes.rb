87613944876a4591749213c6d5204b4bcbcdfd09
josornoc <jose_osorno@hotmail.com>
2015-02-12 09:43:18 +0100

    routes should record like per url

--- course/rails/UrlShortener/config/routes.rb
+++ course/rails/UrlShortener/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   get '/' => 'site#home'
-  root :controller => 'site', :action => 'redirect' 
+  get '/:boo_url' => 'site#redirect'
 
+  #root :controller => 'site', :action => 'redirect' 
   #get '/:boo_url' => 'static', :action => 'site#redirect'
   #get '/:boo_url' => 'site#redirect'
   #get '/:boo_url' => 'site#redirect'
